84ce8a75aa4ed728b476965638df7db9fc14a696
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-05-15 20:30:43 -0300

    feat: create button and method to add product to cart

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,9 +1,12 @@
 <template>
     <div>
         <h1>Product List</h1>
+        <h2>My cart: </h2>
+        <p>{{cart}}</p>
         <ul>
             <li v-for="product in products">
-                {{ product.title }} - {{ product.price }}
+                {{ product.title }} - {{ product.price }} - {{ product.inventory}}
+                <button @click="addProductToCart(product)"> add to cart</button>
             </li>
         </ul>
     </div>
@@ -14,10 +17,18 @@ export default {
     computed: {
         products() {
             return this.$store.getters.availableProducts
+        },
+        cart() {
+            return this.$store.getters.cart
         }
     },
     created() {
         this.$store.dispatch('fetchProducts')
-    }
+    },
+    methods: {
+        addProductToCart(product) {
+            this.$store.dispatch('addProductToCart', product)
+        }
+    },
 }
 </script>
