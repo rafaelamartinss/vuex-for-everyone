9a7fd017b570a8f12b6557fef5d9aca79a62885d
Rafaela Leal <rafaelalealmartinss@gmail.com>
2022-05-15 20:31:03 -0300

    create shopping cart component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import ProductList from './components/ProductList.vue'
+import ShoppingCart from './components/ShoppingCart.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <product-list />
+  <shopping-cart />
 </template>
 
 <style>
